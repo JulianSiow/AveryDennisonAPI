93a017d9e806142df786f66ce16accdb2c4cab79
root <root@Julian-PC.localdomain>
2020-01-30 15:42:45 +0100

    adds Article model

---
+++ Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AveryDennisonAPI.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string ArticleNumber { get; set; }
+        public double SalesPrice { get; set }
+    }
+}
